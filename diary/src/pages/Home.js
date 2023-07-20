895cd7850f32e762a0acd23af1151ea72316a56e
kanggicheon <snoopy1516@naver.com>
2023-07-20 17:59:48 +0900

    Home.js
    초기화 시켜놓기

--- diary/src/pages/Home.js
+++ diary/src/pages/Home.js
@@ -1,17 +1,7 @@
 // 인덱스 페이지 컴포넌트
 
-import Editor from "../component/Editor";
-
 const Home = () => {
-  return (
-    <div>
-      <Editor
-        onSubmit={() => {
-          alert("작성 완료 버튼 클릭 테스트");
-        }}
-      />
-    </div>
-  );
+  return <div>Home 페이지</div>;
 };
 
 export default Home;
