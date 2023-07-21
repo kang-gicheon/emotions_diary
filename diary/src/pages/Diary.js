4d06e7ecdf9ed87603da8e0a114fa61690c16fb4
kanggicheon <snoopy1516@naver.com>
2023-07-21 23:45:25 +0900

    URl 생성

--- diary/src/pages/Diary.js
+++ diary/src/pages/Diary.js
@@ -1,8 +1,10 @@
 // 일기 상세 조회 페이지 컴포넌트
-import { useParams } from "react-router-dom";
+import { useParams } from 'react-router-dom';
+import useDiary from '../hooks/useDiary';
 
 const Diary = () => {
   const { id } = useParams();
+  const data = useDiary(id); // 인수를 통해 URL파라미터로 받은 일기 id 전달
 
   return (
     <div>
