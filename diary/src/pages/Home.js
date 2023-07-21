fda3a8cf75a8516a7bb276dd5ad5fbdd95667106
kanggicheon <snoopy1516@naver.com>
2023-07-21 16:40:35 +0900

    헤더 구현하기
    1. 버튼 클릭 유무에 따라 1달 전, 뒤로 가능 기능
    2. 현재 날짜 연도, 월 보여줌

--- diary/src/pages/Home.js
+++ diary/src/pages/Home.js
@@ -1,7 +1,36 @@
 // 인덱스 페이지 컴포넌트
+import { useState } from "react";
+import Button from "../component/Button";
+import Header from "../component/Header";
 
 const Home = () => {
-  return <div>Home 페이지</div>;
+  // 헤더에 표시할 날짜를 저장할 state
+  const [pivotDate, setPivotDate] = useState(new Date());
+
+  // pivotDate에 저장된 날짜(Date)객체를 yyyy년 mm월 형식의 문자열로 제작
+  const headerTitle = `${pivotDate.getFullYear()}년 ${
+    pivotDate.getMonth() + 1
+  }월`;
+
+  // 월 증가 함수
+  const onIncreaseMonth = () => {
+    setPivotDate(new Date(pivotDate.getFullYear(), pivotDate.getMonth() + 1));
+  };
+
+  // 월 감소 함수
+  const onDecreaseMonth = () => {
+    setPivotDate(new Date(pivotDate.getFullYear(), pivotDate.getMonth() - 1));
+  };
+
+  return (
+    <div>
+      <Header
+        title={headerTitle}
+        leftChild={<Button text={"<"} onClick={onDecreaseMonth} />} // 버튼을 클릭시 한달씩 뒤로
+        rightChild={<Button text={">"} onClick={onIncreaseMonth} />} // 버튼을 클릭시 한달씩 앞으로
+      />
+    </div>
+  );
 };
 
 export default Home;
