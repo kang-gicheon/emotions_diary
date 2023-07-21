c3b941178e0a09ee9de3755e76e5436250159d3b
kanggicheon <snoopy1516@naver.com>
2023-07-21 23:12:57 +0900

    Home 페이지 구현 끝
    일기 리스트, 리스트 상세조회 이동 기능 , 수정하기 이동 기능 home에 적용

--- diary/src/component/DiaryItem.js
+++ diary/src/component/DiaryItem.js
@@ -1,9 +1,43 @@
 // 작성된 일기 리스트, 아이템을 보여줄 컴포넌트
 
+import { useNavigate } from 'react-router-dom';
 import './DiaryItem.css';
+import { getEmotionImgById } from '../util';
+import Button from './Button';
 
 const DiaryItem = ({ id, emotionId, content, date }) => {
-  return <div className="DiaryItem">{content}</div>;
+  const navigate = useNavigate();
+  const goDetail = () => {
+    // 이미지 섹션을 클릭할시 해당 일기를 상세 조회하는 페이지로 이동
+    navigate(`/diary/${id}`);
+  };
+  //  버튼 섹션 = 해당 일기의 Edit 페이지로 이동(추가 기능 - 수정하기)
+  const goEdit = () => {
+    navigate(`/edit/${id}`);
+  };
+
+  return (
+    <div className="DiaryItem">
+      <div
+        onClick={goDetail} // 렌더링할 이미지 섹션
+        className={['img_section', `img_section_${emotionId}`].join(' ')}
+      >
+        <img alt={`emotion${emotionId}`} src={getEmotionImgById(emotionId)} />
+      </div>
+      {/* 일기 정보 섹션, 클릭시 일기 상세 조회 Diary 페이지로 이동 */}
+      <div onClick={goDetail} className="info_section">
+        <div className="date_wrapper">
+          {/* 일기 작성일 표시, 문자열 date 숫자형 Date객체로 변환 */}
+          {new Date(parseInt(date)).toLocaleDateString()}
+        </div>
+        {/* 일기 내용은 25자까지만 표시 */}
+        <div className="content_wrapper">{content.slice(0, 25)}</div>
+      </div>
+      <div className="button_section">
+        <Button onClick={goEdit} text={'수정하기'} />
+      </div>
+    </div>
+  );
 };
 
 export default DiaryItem;
