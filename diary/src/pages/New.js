0ad23073f163854048071abe2fd5ebf820333ec7
kanggicheon <snoopy1516@naver.com>
2023-07-22 00:58:11 +0900

    새로운 일기 작성(New.js) 구현 끝 - Create

--- diary/src/pages/New.js
+++ diary/src/pages/New.js
@@ -1,6 +1,32 @@
 // 새로운 일기 작성 페이지 컴포넌트
 
+import { useNavigate } from 'react-router-dom';
+import Header from '../component/Header';
+import Button from '../component/Button';
+import Editor from '../component/Editor';
+import { useContext } from 'react';
+import { DiaryDispatchContext } from '../App';
+
 const New = () => {
-  return <div>New 페이지</div>;
+  const { onCreate } = useContext(DiaryDispatchContext); // useContext를 호출해 DiaryDispatchContext에서 onCreate 호출
+  const navigate = useNavigate();
+  const goBack = () => {
+    navigate(-1);
+  };
+  const onSubmit = (data) => {
+    const { date, content, emotionId } = data;
+    onCreate(date, content, emotionId);
+    navigate('/', { replace: true });
+  };
+
+  return (
+    <div>
+      <Header
+        title={'새 일기 쓰기'}
+        leftChild={<Button text={'< 뒤로 가기'} onClick={goBack} />}
+      />
+      <Editor onSubmit={onSubmit} />
+    </div>
+  );
 };
 export default New;
