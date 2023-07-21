51f8c4ffe0d4d45bea2be9979c97bf5d66333808
kanggicheon <snoopy1516@naver.com>
2023-07-21 17:55:04 +0900

    새로운 일기 쓰기 버튼 구현
    버튼 클릭시 new 페이지로 이동하게 제작
    router 이용

--- diary/src/component/DiaryList.js
+++ diary/src/component/DiaryList.js
@@ -1,18 +1,49 @@
-// 일기리스트 하단부 구현
+// 일기 목록, 정렬 기능을 포함한 컴포넌트
 
+import { useState } from "react";
 import Button from "./Button";
 import "./DiaryList.css";
+import { useNavigate } from "react-router-dom";
+
+const sortOptionList = [
+  { value: "latest", name: "최신순" },
+  { value: "oldest", name: "오래된 순" },
+];
 
 // 일기 리스트 렌더링을 위한 부모인 home에서 필터링 된 일기를 props로
 const DiaryList = ({ data }) => {
+  const [sortType, setSortType] = useState("latest"); // 사용자가 선택할 정렬기준 state 초기값은 최신순으로
+
+  const navigate = useNavigate();
+
+  const onClickNew = () => {
+    // 버튼을 클릭시 new 페이지로 이동하는 함수(새로운 일기 작성)
+    navigate("/new");
+  };
+
+  const onChangeSortType = (e) => {
+    // 정렬 기준 변경기 새 기준으로 sortType 업데이트 하는 이벤트
+    setSortType(e.target.value);
+  };
+
   return (
     <div className="DiaryList">
       <div className="menu_wrapper">
         <div className="left_col">
-          <select></select>
+          <select value={sortType} onChange={onChangeSortType}>
+            {sortOptionList.map((it, idx) => (
+              <option key={idx} value={it.value}>
+                {it.name}
+              </option>
+            ))}
+          </select>
         </div>
         <div className="right_col">
-          <Button type={"positive"} text={"새 일기 쓰기"} />
+          <Button
+            type={"positive"}
+            text={"새 일기 쓰기"}
+            onClick={onClickNew}
+          />
         </div>
       </div>
     </div>
